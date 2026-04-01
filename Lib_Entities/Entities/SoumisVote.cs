8173776c56b0b486f03ad330af7af1dbaaf71423
benjaminlrl <benjamin.lorieul.pro@gmail.com>
2026-04-01 16:48:18 +0200

    feat(votes): entité SoumisVote

---
+++ Lib_Entities/Entities/SoumisVote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_Entities.Entities
+{
+    public class SoumisVote
+    {
+        public DateTime dateDebutVote { get; set; }
+        public DateTime dateFinVote { get; set; }
+        public int IdJeu { get; set; }
+        public int IdPlateforme { get; set; }
+        public Plateforme Plateforme { get; set; }
+        public Jeu Jeu { get; set; }
+    }
+}
