020d212b27fb5ab97169ab52b99c0f4197635183
benjaminlrl <benjamin.lorieul.pro@gmail.com>
2026-04-01 17:24:03 +0200

    feat(votes): service et interface dédiés au SoumisVote

---
+++ Lib_Services/Interfaces/ISoumisVote.cs
@@ -0,0 +1,18 @@
+﻿using Lib_Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_Services.Interfaces
+{
+    public interface ISoumisVoteService
+    {
+        void Creer(SoumisVote soumisVote);
+        void Modifier(SoumisVote soumisVote);
+        void Supprimer(int idJeu, int idPlateforme);
+        List<SoumisVote> Lister(string filtre);
+        SoumisVote? Obtenir(int idJeu, int idPlateforme);
+
+        List<string> ValiderSoumisVote(SoumisVote soumisVote);
+    }
+}
