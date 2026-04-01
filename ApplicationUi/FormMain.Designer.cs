28857218a843f705953790d486f27d2cf2b4199e
benjaminlrl <benjamin.lorieul.pro@gmail.com>
2026-04-01 20:42:58 +0200

    feat(voter): ajout du user contol dédiés à voter

--- ApplicationUi/FormMain.Designer.cs
+++ ApplicationUi/FormMain.Designer.cs
@@ -24,14 +24,15 @@ namespace ApplicationUi
             btnPostes = new Button();
             btnEspaces = new Button();
             btnTournois = new Button();
-            btnLotComposants = new Button();
             buttonJeux = new Button();
             btnOrganisateurs = new Button();
+            btnLots = new Button();
+            btnLotComposants = new Button();
             btnQuitter = new Button();
             panelHeader = new Panel();
             lblTitre = new Label();
             panelContent = new Panel();
-            btnLots = new Button();
+            buttonVoter = new Button();
             panelMenu.SuspendLayout();
             panelHeader.SuspendLayout();
             SuspendLayout();
@@ -47,13 +48,14 @@ namespace ApplicationUi
             panelMenu.Controls.Add(btnOrganisateurs);
             panelMenu.Controls.Add(btnLots);
             panelMenu.Controls.Add(btnLotComposants);
+            panelMenu.Controls.Add(buttonVoter);
             panelMenu.Controls.Add(btnQuitter);
             panelMenu.Dock = DockStyle.Left;
             panelMenu.FlowDirection = FlowDirection.TopDown;
             panelMenu.Location = new Point(0, 0);
-            panelMenu.Margin = new Padding(3, 4, 3, 4);
+            panelMenu.Margin = new Padding(4, 5, 4, 5);
             panelMenu.Name = "panelMenu";
-            panelMenu.Size = new Size(229, 881);
+            panelMenu.Size = new Size(286, 1101);
             panelMenu.TabIndex = 2;
             panelMenu.WrapContents = false;
             // 
@@ -66,10 +68,10 @@ namespace ApplicationUi
             btnPlateformes.ForeColor = Color.White;
             btnPlateformes.Image = Properties.Resources.plateforme;
             btnPlateformes.ImageAlign = ContentAlignment.MiddleLeft;
-            btnPlateformes.Location = new Point(0, 65);
-            btnPlateformes.Margin = new Padding(0, 65, 0, 7);
+            btnPlateformes.Location = new Point(0, 81);
+            btnPlateformes.Margin = new Padding(0, 81, 0, 9);
             btnPlateformes.Name = "btnPlateformes";
-            btnPlateformes.Size = new Size(229, 65);
+            btnPlateformes.Size = new Size(286, 81);
             btnPlateformes.TabIndex = 0;
             btnPlateformes.Text = "      Plateformes";
             btnPlateformes.UseVisualStyleBackColor = false;
@@ -84,10 +86,10 @@ namespace ApplicationUi
             btnPostes.ForeColor = Color.White;
             btnPostes.Image = Properties.Resources.postes;
             btnPostes.ImageAlign = ContentAlignment.MiddleLeft;
-            btnPostes.Location = new Point(0, 146);
-            btnPostes.Margin = new Padding(0, 9, 0, 7);
+            btnPostes.Location = new Point(0, 182);
+            btnPostes.Margin = new Padding(0, 11, 0, 9);
             btnPostes.Name = "btnPostes";
-            btnPostes.Size = new Size(239, 67);
+            btnPostes.Size = new Size(299, 84);
             btnPostes.TabIndex = 1;
             btnPostes.Text = "        Postes de jeu";
             btnPostes.UseVisualStyleBackColor = false;
@@ -103,10 +105,10 @@ namespace ApplicationUi
             btnEspaces.ForeColor = Color.White;
             btnEspaces.Image = Properties.Resources.espace;
             btnEspaces.ImageAlign = ContentAlignment.MiddleLeft;
-            btnEspaces.Location = new Point(0, 229);
-            btnEspaces.Margin = new Padding(0, 9, 0, 7);
+            btnEspaces.Location = new Point(0, 286);
+            btnEspaces.Margin = new Padding(0, 11, 0, 9);
             btnEspaces.Name = "btnEspaces";
-            btnEspaces.Size = new Size(229, 65);
+            btnEspaces.Size = new Size(286, 81);
             btnEspaces.TabIndex = 2;
             btnEspaces.Text = "Espaces";
             btnEspaces.UseVisualStyleBackColor = false;
@@ -122,33 +124,14 @@ namespace ApplicationUi
             btnTournois.ForeColor = Color.White;
             btnTournois.Image = Properties.Resources.tournoi;
             btnTournois.ImageAlign = ContentAlignment.MiddleLeft;
-            btnTournois.Location = new Point(0, 310);
-            btnTournois.Margin = new Padding(0, 9, 0, 7);
+            btnTournois.Location = new Point(0, 387);
+            btnTournois.Margin = new Padding(0, 11, 0, 9);
             btnTournois.Name = "btnTournois";
-            btnTournois.Size = new Size(229, 65);
+            btnTournois.Size = new Size(286, 81);
             btnTournois.TabIndex = 3;
             btnTournois.Text = "Tournois";
             btnTournois.UseVisualStyleBackColor = false;
             btnTournois.Click += btnTournois_Click;
-            // 
-            // btnLotComposants
-            // 
-            btnLotComposants.BackColor = Color.Blue;
-            btnLotComposants.BackgroundImageLayout = ImageLayout.None;
-            btnLotComposants.FlatAppearance.BorderSize = 0;
-            btnLotComposants.FlatStyle = FlatStyle.Flat;
-            btnLotComposants.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
-            btnLotComposants.ForeColor = Color.White;
-            btnLotComposants.Image = Properties.Resources.tournoi;
-            btnLotComposants.ImageAlign = ContentAlignment.MiddleLeft;
-            btnLotComposants.Location = new Point(0, 634);
-            btnLotComposants.Margin = new Padding(0, 9, 0, 4);
-            btnLotComposants.Name = "btnLotComposants";
-            btnLotComposants.Size = new Size(225, 91);
-            btnLotComposants.TabIndex = 6;
-            btnLotComposants.Text = "      Composant des lots";
-            btnLotComposants.UseVisualStyleBackColor = false;
-            btnLotComposants.Click += btnLotComposants_Click;
             // 
             // buttonJeux
             // 
@@ -160,10 +143,10 @@ namespace ApplicationUi
             buttonJeux.ForeColor = Color.White;
             buttonJeux.Image = Properties.Resources.espace;
             buttonJeux.ImageAlign = ContentAlignment.MiddleLeft;
-            buttonJeux.Location = new Point(0, 391);
-            buttonJeux.Margin = new Padding(0, 9, 0, 7);
+            buttonJeux.Location = new Point(0, 488);
+            buttonJeux.Margin = new Padding(0, 11, 0, 9);
             buttonJeux.Name = "buttonJeux";
-            buttonJeux.Size = new Size(229, 65);
+            buttonJeux.Size = new Size(286, 81);
             buttonJeux.TabIndex = 4;
             buttonJeux.Text = "Jeux";
             buttonJeux.UseVisualStyleBackColor = false;
@@ -179,14 +162,51 @@ namespace ApplicationUi
             btnOrganisateurs.ForeColor = Color.White;
             btnOrganisateurs.Image = Properties.Resources.tournoi;
             btnOrganisateurs.ImageAlign = ContentAlignment.MiddleLeft;
-            btnOrganisateurs.Location = new Point(0, 472);
-            btnOrganisateurs.Margin = new Padding(0, 9, 0, 7);
+            btnOrganisateurs.Location = new Point(0, 589);
+            btnOrganisateurs.Margin = new Padding(0, 11, 0, 9);
             btnOrganisateurs.Name = "btnOrganisateurs";
-            btnOrganisateurs.Size = new Size(229, 65);
+            btnOrganisateurs.Size = new Size(286, 81);
             btnOrganisateurs.TabIndex = 5;
             btnOrganisateurs.Text = "      Organisateurs";
             btnOrganisateurs.UseVisualStyleBackColor = false;
             btnOrganisateurs.Click += btnOrganisateurs_Click;
+            // 
+            // btnLots
+            // 
+            btnLots.BackColor = Color.FromArgb(128, 128, 255);
+            btnLots.BackgroundImageLayout = ImageLayout.None;
+            btnLots.FlatAppearance.BorderSize = 0;
+            btnLots.FlatStyle = FlatStyle.Flat;
+            btnLots.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            btnLots.ForeColor = Color.White;
+            btnLots.Image = Properties.Resources.tournoi;
+            btnLots.ImageAlign = ContentAlignment.MiddleLeft;
+            btnLots.Location = new Point(0, 690);
+            btnLots.Margin = new Padding(0, 11, 0, 9);
+            btnLots.Name = "btnLots";
+            btnLots.Size = new Size(286, 81);
+            btnLots.TabIndex = 7;
+            btnLots.Text = "Lots";
+            btnLots.UseVisualStyleBackColor = false;
+            // 
+            // btnLotComposants
+            // 
+            btnLotComposants.BackColor = Color.Blue;
+            btnLotComposants.BackgroundImageLayout = ImageLayout.None;
+            btnLotComposants.FlatAppearance.BorderSize = 0;
+            btnLotComposants.FlatStyle = FlatStyle.Flat;
+            btnLotComposants.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            btnLotComposants.ForeColor = Color.White;
+            btnLotComposants.Image = Properties.Resources.tournoi;
+            btnLotComposants.ImageAlign = ContentAlignment.MiddleLeft;
+            btnLotComposants.Location = new Point(0, 791);
+            btnLotComposants.Margin = new Padding(0, 11, 0, 5);
+            btnLotComposants.Name = "btnLotComposants";
+            btnLotComposants.Size = new Size(281, 114);
+            btnLotComposants.TabIndex = 6;
+            btnLotComposants.Text = "      Composant des lots";
+            btnLotComposants.UseVisualStyleBackColor = false;
+            btnLotComposants.Click += btnLotComposants_Click;
             // 
             // btnQuitter
             // 
@@ -197,10 +217,10 @@ namespace ApplicationUi
             btnQuitter.ForeColor = Color.White;
             btnQuitter.Image = Properties.Resources.deconnecter;
             btnQuitter.ImageAlign = ContentAlignment.MiddleLeft;
-            btnQuitter.Location = new Point(3, 733);
-            btnQuitter.Margin = new Padding(3, 4, 3, 4);
+            btnQuitter.Location = new Point(4, 1016);
+            btnQuitter.Margin = new Padding(4, 5, 4, 5);
             btnQuitter.Name = "btnQuitter";
-            btnQuitter.Size = new Size(229, 65);
+            btnQuitter.Size = new Size(286, 81);
             btnQuitter.TabIndex = 6;
             btnQuitter.Text = "  Quitter";
             btnQuitter.UseVisualStyleBackColor = false;
@@ -213,19 +233,20 @@ namespace ApplicationUi
             panelHeader.Dock = DockStyle.Top;
             panelHeader.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
             panelHeader.ForeColor = SystemColors.ControlLightLight;
-            panelHeader.Location = new Point(229, 0);
-            panelHeader.Margin = new Padding(3, 4, 3, 4);
+            panelHeader.Location = new Point(286, 0);
+            panelHeader.Margin = new Padding(4, 5, 4, 5);
             panelHeader.Name = "panelHeader";
-            panelHeader.Size = new Size(996, 65);
+            panelHeader.Size = new Size(1245, 81);
             panelHeader.TabIndex = 1;
             // 
             // lblTitre
             // 
             lblTitre.AutoSize = true;
             lblTitre.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
-            lblTitre.Location = new Point(23, 16);
+            lblTitre.Location = new Point(29, 20);
+            lblTitre.Margin = new Padding(4, 0, 4, 0);
             lblTitre.Name = "lblTitre";
-            lblTitre.Size = new Size(349, 32);
+            lblTitre.Size = new Size(401, 38);
             lblTitre.TabIndex = 0;
             lblTitre.Text = "Espace de Gestion du Festival";
             // 
@@ -233,42 +254,43 @@ namespace ApplicationUi
             // 
             panelContent.BackColor = Color.FromArgb(245, 246, 248);
             panelContent.Dock = DockStyle.Fill;
-            panelContent.Location = new Point(229, 65);
-            panelContent.Margin = new Padding(3, 4, 3, 4);
+            panelContent.Location = new Point(286, 81);
+            panelContent.Margin = new Padding(4, 5, 4, 5);
             panelContent.Name = "panelContent";
-            panelContent.Size = new Size(996, 816);
+            panelContent.Size = new Size(1245, 1020);
             panelContent.TabIndex = 0;
             // 
-            // btnLots
+            // buttonVoter
             // 
-            btnLots.BackColor = Color.FromArgb(128, 128, 255);
-            btnLots.BackgroundImageLayout = ImageLayout.None;
-            btnLots.FlatAppearance.BorderSize = 0;
-            btnLots.FlatStyle = FlatStyle.Flat;
-            btnLots.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
-            btnLots.ForeColor = Color.White;
-            btnLots.Image = Properties.Resources.tournoi;
-            btnLots.ImageAlign = ContentAlignment.MiddleLeft;
-            btnLots.Location = new Point(0, 553);
-            btnLots.Margin = new Padding(0, 9, 0, 7);
-            btnLots.Name = "btnLots";
-            btnLots.Size = new Size(229, 65);
-            btnLots.TabIndex = 7;
-            btnLots.Text = "Lots";
-            btnLots.UseVisualStyleBackColor = false;
+            buttonVoter.BackColor = Color.FromArgb(0, 192, 0);
+            buttonVoter.BackgroundImageLayout = ImageLayout.None;
+            buttonVoter.FlatAppearance.BorderSize = 0;
+            buttonVoter.FlatStyle = FlatStyle.Flat;
+            buttonVoter.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            buttonVoter.ForeColor = Color.White;
+            buttonVoter.Image = Properties.Resources.tournoi;
+            buttonVoter.ImageAlign = ContentAlignment.MiddleLeft;
+            buttonVoter.Location = new Point(0, 921);
+            buttonVoter.Margin = new Padding(0, 11, 0, 9);
+            buttonVoter.Name = "buttonVoter";
+            buttonVoter.Size = new Size(286, 81);
+            buttonVoter.TabIndex = 8;
+            buttonVoter.Text = "Voter";
+            buttonVoter.UseVisualStyleBackColor = false;
+            buttonVoter.Click += buttonVoter_Click;
             // 
             // FormMain
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(245, 246, 248);
-            ClientSize = new Size(1225, 881);
+            ClientSize = new Size(1531, 1101);
             ControlBox = false;
             Controls.Add(panelContent);
             Controls.Add(panelHeader);
             Controls.Add(panelMenu);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 4, 3, 4);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "FormMain";
             Text = "Back-office organisateur";
             WindowState = FormWindowState.Maximized;
@@ -294,5 +316,6 @@ namespace ApplicationUi
         private Button btnLotComposants;
         private Button buttonJeux;
         private Button btnLots;
+        private Button buttonVoter;
     }
 }
---
+++ ApplicationUi/UcVoter.cs
@@ -0,0 +1,218 @@
+﻿using Lib_Entities.Entities;
+using Lib_Metier.Data.Configurations;
+using Lib_Services.Interfaces;
+using Lib_Services.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ApplicationUi
+{
+    public partial class UcVoter : UserControl
+    {
+        private readonly ITournoiService _serviceTournoi;
+        private readonly IOrganisateurService _serviceOrganisateur;
+        private readonly IJeuService _serviceJeu;
+        private readonly IVoterService _serviceVoter;
+        private readonly IPlateformeService _servicePlateforme;
+        private bool fonctionnelSelectionne;
+        private Jeu? _jeuSelectionne;
+        private Voter? _voterSelectionne;
+        private string filtre;
+        private string ordreChamp;
+        private readonly Organisateur _organisateurConnecte;
+        public UcVoter(Organisateur unOrganisateurConnecte)
+        {
+            InitializeComponent();
+            var context = new ApplicationDbContext();
+            _serviceTournoi = new TournoiService(context);
+            _serviceOrganisateur = new OrganisateurService(context);
+            _serviceJeu = new JeuService(context);
+            _servicePlateforme = new PlateformeService(context);
+            _serviceVoter = new VoterService(context);
+            _jeuSelectionne = null;
+            _voterSelectionne = null;
+            fonctionnelSelectionne = false;
+            filtre = "";
+            ordreChamp = "ASC";
+            _organisateurConnecte = unOrganisateurConnecte;
+            ChargerJeux();
+            buttonSupprimer.Enabled = _jeuSelectionne != null && _voterSelectionne != null;
+            buttonEffacer.Text = " 🧽  Effacer";
+            if (_serviceOrganisateur.estAutoriser(_organisateurConnecte, Organisateur.LesUC.UcPostesDeJeu, "Ajouter") == false)
+            {
+                buttonVoter.Visible = false;
+            }
+            if (_serviceOrganisateur.estAutoriser(_organisateurConnecte, Organisateur.LesUC.UcPostesDeJeu, "Supprimer") == false)
+            {
+                buttonSupprimer.Visible = false;
+            }
+            // TODO: Ajouter un tooltip sur les boutons pour expliquer leur fonction à l'utilisateur
+            // TODO: Ajouter un graphique pour indiquer le nombre de postes de jeu
+            // fonctionnels vs non fonctionnels
+            // TODO: ajouter une option de filtrage croissant décroissant sur la référence du poste de jeu
+        }
+
+        #region Evènements
+        private void ChargerJeux()
+        {
+            dataGridJeux.DataSource = null;
+            dataGridJeux.DataSource = _serviceJeu.Lister(filtre);
+            MEP_DataGrid();
+        }
+        private void ChargerPlateformes()
+        {
+            comboBoxPlateforme.DataSource = null;
+            comboBoxPlateforme.DataSource = _jeuSelectionne.Plateformes.ToList();
+            // charge les plateformes dans le comboBox et affiche le libellé tout en conservant l'id en valeur
+            comboBoxPlateforme.DisplayMember = "Libelle";
+            comboBoxPlateforme.ValueMember = "IdPlateforme";
+        }
+
+        /// <summary>
+        /// Resets all tournament-related input fields and controls to their default values.
+        /// </summary>
+        /// <remarks>Use this method to clear the current tournament selection and prepare the form for
+        /// entering a new tournament. All user input fields are cleared or set to their minimum values, and the status
+        /// controls are reset to indicate a planned tournament.</remarks>
+        private void Raz_Zones()
+        {
+            textBoxTitre.Clear();
+            textBoxDescription.Clear();
+            comboBoxPlateforme.SelectedItem = null;
+            textBoxPegi.Clear();
+            _jeuSelectionne = null;
+            buttonSupprimer.Enabled = _jeuSelectionne != null;
+        }
+        private void MEP_DataGrid()
+        {
+            dataGridJeux.Columns["Titre"].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridJeux.Columns["IdJeu"].Visible = false;
+            dataGridJeux.Columns["Tournois"].Visible = false;
+            dataGridJeux.Columns["Plateformes"].Visible = false;
+        }
+
+        private void dataGridJeux_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // on ne gère le clic que sur les lignes, pas sur les en-têtes
+            // Ignorer les clics sur l'en-tête (gérés pour le tri)
+            // Gérer le trie par ordre des champs en fonction du clique sur la cellule d'en-tête
+            if (e.RowIndex < 0)
+            {
+                // on né gère pas les cliques sur la première colonne
+                if (e.ColumnIndex < 1)
+                    return;
+
+                var donnees = _serviceJeu.Lister(filtre);
+
+                // Utiliser un dictionnaire plutôt qu'un switch pour associer les index de colonnes
+                // à des fonctions de sélection de clé
+                var map = new Dictionary<int, Func<Jeu, object>>
+                {
+                    {dataGridJeux.Columns["Titre"].Index, j => j.Titre},
+                    {dataGridJeux.Columns["Description"].Index, j => j.Description},
+                    {dataGridJeux.Columns["Pegi"].Index, j => j.Pegi},
+                    {dataGridJeux.Columns["AnneeSortie"].Index, j => j.AnneeSortie},
+                    {dataGridJeux.Columns["DateSortie"].Index, j => j.DateSortie},
+                    {dataGridJeux.Columns["Editeur"].Index, j => j.Editeur},
+                };
+
+                if (!map.TryGetValue(e.ColumnIndex, out var keySelector))
+                    return;
+
+                dataGridJeux.DataSource = ordreChamp == "ASC"
+                    ? donnees.OrderByDescending(keySelector).ToList()
+                    : donnees.OrderBy(keySelector).ToList();
+                // permute l'ordre du champ
+                ordreChamp = ordreChamp == "ASC" ? "DESC" : "ASC";
+
+                MEP_DataGrid();
+                return;
+            }
+
+            _jeuSelectionne = dataGridJeux.Rows[e.RowIndex].DataBoundItem as Jeu;
+
+            if (_jeuSelectionne != null)
+                RemplirFormulaire(_jeuSelectionne);
+
+            buttonSupprimer.Enabled = _jeuSelectionne != null;
+
+        }
+
+        private void RemplirFormulaire(Jeu jeu)
+        {
+            textBoxTitre.Text = jeu.Titre;
+            textBoxDescription.Text = jeu.Description;
+            textBoxEditeur.Text = jeu.Editeur;
+            textBoxPegi.Text = jeu.Pegi.ToString();
+            ChargerPlateformes();
+            dateTimePickerDateSortie.Value = jeu.DateSortie;
+        }
+
+        /// <summary>
+        /// Permet de filtrer les postes de jeu affichés dans le DataGrid en 
+        /// fonction du texte saisi dans la zone de recherche.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxRecherche_TextChanged(object sender, EventArgs e)
+        {
+            filtre = textBoxRecherche.Text;
+            ChargerJeux();
+        }
+        #endregion
+
+        #region Validations
+        private bool ValiderJeu(Jeu jeu)
+        {
+            var erreurs = _serviceJeu.ValiderJeu(jeu);
+            if (erreurs.Any())
+            {
+                MessageBox.Show(string.Join("\n", erreurs));
+                return false;
+            }
+            return true;
+        }
+        #endregion
+
+        #region Boutons
+        public void buttonVoter_Click(object sender, EventArgs e)
+        {
+            var voter = new Voter
+            {
+                IdJeu = _jeuSelectionne.IdJeu,
+                IdPlateforme = _voterSelectionne.Plateforme.IdPlateforme,
+                IdUser = 1,//TODO: Récupérer l'ID de l'utilisateur connecté
+                Plateforme = _voterSelectionne.Plateforme,
+                Jeu = _jeuSelectionne,
+                DateVote = DateTime.Now
+
+            };
+        }
+        private void buttonEffacer_Click(object sender, EventArgs e)
+        {
+            Raz_Zones();
+        }
+        private void buttonSupprimer_Click(object sender, EventArgs e)
+        {
+            if (dataGridJeux.CurrentRow == null)
+                return;
+
+            if (_jeuSelectionne == null || _voterSelectionne == null)
+                return;
+            var jeu = (Jeu)dataGridJeux.CurrentRow.DataBoundItem;
+
+            _serviceJeu.Supprimer(_jeuSelectionne.IdJeu);
+            _jeuSelectionne = null;
+            ChargerJeux();
+            Raz_Zones();
+
+        }
+
+        #endregion
+    }
+}
