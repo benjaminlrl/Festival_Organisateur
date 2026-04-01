72f4649b5188d5762737bcbc1539a82cf3d245dd
benjaminlrl <benjamin.lorieul.pro@gmail.com>
2026-04-01 17:18:42 +0200

    feat(votes): service et interface dédié aux votes

---
+++ Lib_Services/Interfaces/IVoteService.cs
@@ -0,0 +1,18 @@
+﻿using Lib_Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_Services.Interfaces
+{
+    public interface IVoteService
+    {
+        void Creer(Vote vote);
+        void Modifier(Vote vote);
+        void Supprimer(int idUser, int idJeu, int idPlateforme);
+        List<Vote> Lister(string filtre);
+        Vote? Obtenir(int idUser, int idJeu, int idPlateforme);
+
+        List<string> ValiderVote(Vote vote);
+    }
+}
