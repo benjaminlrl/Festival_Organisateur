8cea6f743ce4b101162737761f99639c749e3e8e
benjaminlrl <benjamin.lorieul.pro@gmail.com>
2026-04-01 16:56:41 +0200

    feat(votes): fichier de configuration de la table vote

---
+++ Lib_Entities/Entities/Vote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_Entities.Entities
+{
+    /// <summary>
+    /// Représente un vote,
+    /// Un vote dispose d'un idUser, d'un idJeu, d'un idPlateforme et d'une date de vote.
+    /// 
+    /// </summary>
+    public class Vote
+    {
+        public int IdUser { get; set; }
+        public int IdJeu { get; set; }
+        public int IdPlateforme { get; set; }
+        public DateTime DateVote { get; set; }
+
+        public Plateforme Plateforme { get; set; }
+        public Jeu Jeu { get; set; }
+    }
+}
