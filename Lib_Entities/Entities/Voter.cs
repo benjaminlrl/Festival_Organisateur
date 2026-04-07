efcce5611587f98088b8f41291d6298661bd929f
benjaminlrl <benjamin.lorieul.pro@gmail.com>
2026-04-07 20:24:00 +0200

    feat(voter): ajout de la propriété non mappé NbVotes pour le classement

--- Lib_Entities/Entities/Voter.cs
+++ Lib_Entities/Entities/Voter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Lib_Entities.Entities
@@ -16,19 +17,29 @@ namespace Lib_Entities.Entities
         public int IdPlateforme { get; set; }
         public DateTime DateVote { get; set; }
 
+        [NotMapped]
         public Plateforme Plateforme { get; set; }
         /// <summary>
         /// Propriété récupérée utilisée pour l'affichage (DataGridView).
         /// Renvoie le libellé de la plateforme de façon null-safe ;
         /// retourne chaîne vide si l'espace est null.
         /// </summary>
+        [NotMapped] 
         public string LibellePlateforme => Plateforme?.Libelle ?? string.Empty;
+        [NotMapped]
         public Jeu Jeu { get; set; }
         /// <summary>
         /// Propriété récupérée utilisée pour l'affichage (DataGridView).
         /// Renvoie le titre du jeu de façon null-safe ;
         /// retourne chaîne vide si l'espace est null.
         /// </summary>
+        [NotMapped] 
         public string TitreJeu => Jeu?.Titre ?? string.Empty;
+
+        /// <summary>
+        /// Correspond au nombres de votes associés au binome (id_jeu,id_plateforme) pour le classement des votes
+        /// </summary>
+        [NotMapped]
+        public int NbVotes { get; set; }
     }
 }
