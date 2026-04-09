4766a86ec3ca5561146ed5e7a9b27428cef0a585
Benjamin <159474597+benjaminlrl@users.noreply.github.com>
2026-04-09 20:09:26 +0200

    refactor(votes): transformation de l'espace de vote en espace de gestion des soumisVote (#18)
    * feat(votes): fichier de configuration de la table vote
    
    * feat(votes): service et interface dédié aux votes
    
    * chore(votes): renommage de la table "Votes" en "Voter"
    
    * chore(voter): renommage de vote en voter
    
    * feat(constantes): ajout d'un service dédiés aux constante utilisable dans l'application
    
    * chore(voter): amélioration UX/UI et documentation
    
    * feat(soumisVote): méthodes de statistques
    
    * feat(voter): ajout de la propriété non mappé NbVotes pour le classement
    
    * refactor(soumisVote): formulaire intéractif, ajout et suppression, UX/UI
    
    * feat(soumisVote): controles sur les données associées aux soumisVote
    
    * feat(soumisVote): classement des binomes jeu et plateforme par le nombre de votes associés
    
    * fix(soumisVote): recherche filtrée sur des propriétés non mappées
    
    * fix(soumisVote): tris sur les colonnes de la dataGrid dédiés aux soumisVotes
    
    * fix(soumisVote): controles sur les dates invalides, on peut désormais ajouter un soumisVote ayant en date de début aujourd'hui.
    
    * fix(soumisVote): évenement associé à la barre de recherche
    
    * fix(soumisvote): corrections des contrôles à la modification du soumisVote

--- Lib_Entities/Entities/SoumisVote.cs
+++ Lib_Entities/Entities/SoumisVote.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Lib_Entities.Entities
@@ -11,6 +12,25 @@ namespace Lib_Entities.Entities
         public int IdJeu { get; set; }
         public int IdPlateforme { get; set; }
         public Plateforme Plateforme { get; set; }
+
+        /// <summary>
+        /// Propriété récupérée utilisée pour l'affichage (DataGridView).
+        /// Renvoie le libellé de la plateforme de façon null-safe ;
+        /// retourne chaîne vide si l'espace est null.
+        /// </summary>
+        public string LibellePlateforme => Plateforme?.Libelle ?? string.Empty;
         public Jeu Jeu { get; set; }
+        /// <summary>
+        /// Propriété récupérée utilisée pour l'affichage (DataGridView).
+        /// Renvoie le titre du jeu de façon null-safe ;
+        /// retourne chaîne vide si l'espace est null.
+        /// </summary>
+        public string TitreJeu => Jeu?.Titre ?? string.Empty;
+
+        /// <summary>
+        /// Correspond au taux de vote d'un jeu
+        /// </summary>
+        [NotMapped]
+        public double TauxVoteJeu { get; set; }
     }
 }
